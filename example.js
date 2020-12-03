6021d148eecf016d4badbd427b293fbe4a801121
Chad March <cmarch@resourcedata.com>
2020-12-02 16:12:49 -0800

    New methods to get cycle codes and effective dates

--- example.js
+++ example.js
@@ -1,5 +1,53 @@
 const chartSupplements = require('./')
 
-chartSupplements.list(['PANC', 'PADK']).then(results => {
+// Should log current cycle effective object
+chartSupplements.fetchCycle().then(c => {
+  console.log('current cycle effective object, no param')
+  console.dir(c)
+})
+
+// Should log current cycle effective object
+chartSupplements.fetchCycle('Current').then(c => {
+  console.log('current cycle effective object, with param')
+  console.dir(c)
+})
+
+// Should log next cycle effective object
+chartSupplements.fetchCycle('Next').then(c => {
+  console.log('next cycle effective object, with param')
+  console.dir(c)
+})
+
+// Should log current cycle code
+chartSupplements.fetchCurrentCycleCode().then(c => {
+  console.log('current cycle code')
+  console.log(c)
+})
+
+// Should log the current cycle effective dates
+chartSupplements.getCycleEffectiveDates().then(c => {
+  console.log('the current cycle effective dates, no param')
+  console.log(c)
+})
+// Should log the current cycle effective dates
+chartSupplements.getCycleEffectiveDates('Current').then(c => {
+  console.log('the current cycle effective dates, with param')
+  console.log(c)
+})
+// Should log the next cycle effective dates
+chartSupplements.getCycleEffectiveDates('Next').then(c => {
+  console.log('the next cycle effective dates')
+  console.log(c)
+})
+
+// Should log the current cycle effective dates
+chartSupplements.currentCycleEffectiveDates().then(c => {
+  console.log('the current cycle effective dates')
+  console.log(c)
+})
+
+// The `getNextCycle` option will get the next cycle if it is available when set to true
+//  If it is omitted or set to false, the current cycle will be queried
+chartSupplements.list(['PANC', 'PADK'], { getNextCycle: true, }).then(results => {
   console.log(JSON.stringify(results, null, 2))
 })
--- index.js
+++ index.js
@@ -3,40 +3,182 @@ const cheerio = require('cheerio')
 
 const BASE_URL = 'https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/search/'
 
+// For some reason the server takes forever to respond without this request header
+const ACCEPT = 'text/html'
+
+const defaultQueryOptions = {
+  getNextCycle: false // `false` do not get the 'Next' cycle, only get the 'Current' cycle; `true` get the 'Next' cycle if available
+}
+
 /**
  * Provide a shortcut to the list method
  */
-const chartSupplements = module.exports = (icaos, options = {}) => {
+const chartSupplements = module.exports = (icaos, options = defaultQueryOptions) => {
   return chartSupplements.list(icaos, options)
 }
 
 /**
  * Main listing method; accepts one or more ICAO codes
  */
-chartSupplements.list = (icaos, options = {}) => {
+chartSupplements.list = (icaos, options = defaultQueryOptions) => {
   if (Array.isArray(icaos)) {
-    return Promise.all(icaos.map(listOne))
+    return Promise.all(icaos.map(icao => listOne(icao, options)))
   }
-  return listOne(icaos)
+  return listOne(icaos, options)
+}
+
+chartSupplements.currentCycleEffectiveDates = async () => {
+  const response = await superagent
+    .get(BASE_URL)
+    .set('Accept', ACCEPT)
+  
+  const $ = cheerio.load(response.text)
+  var currentCycle = $('select#cycle > option:contains(Current)').text()
+  return parseEffectiveDates(currentCycle.replace(/(\n|\t)/gm, ''))
 }
 
 /**
- * Determine the current documents cycle number
+ * Returns the text and values of the targeted <select/> element
+ * @param {string} cycle - The target cycle to retrieve. Valid values are 'Current' or 'Next'
  */
-const fetchCurrentCycle = chartSupplements.fetchCurrentCycle = () => superagent.get(BASE_URL)
-  .then(res => {
-    const $ = cheerio.load(res.text)
-    return $('select#cycle > option:contains(Current)').val()
-  })
+const fetchCycle = async (cycle = 'Current') => {
+  // Only all the values 'Current' or 'Next'
+  if (cycle !== 'Current' && cycle !== 'Next') {
+    cycle = 'Current'
+  }
+  const response = await superagent
+    .get(BASE_URL)
+    .set('Accept', ACCEPT)
+  
+  const $ = cheerio.load(response.text)
+  const $cycle = $(`select#cycle > option:contains(${cycle})`)
+  if (!$cycle) {
+    return $cycle
+  }
+  return {
+    text: $cycle.text(),
+    val: $cycle.val()
+  }
+}
+
+chartSupplements.fetchCycle = fetchCycle
+
+chartSupplements.getCycleEffectiveDates = async (cycle = 'Current') => {
+  const { text: currentCycle, } = await fetchCycle(cycle)
+  return parseEffectiveDates(currentCycle.replace(/(\n|\t)/gm, ''))
+} 
+
+chartSupplements.currentCycleEffectiveDates = async () => {
+  const { text: currentCycle, } = await fetchCycle()
+  if (!currentCycle) {
+    console.warn('Could not retrieve current cycle effective dates')
+    return
+  }
+  return parseEffectiveDates(currentCycle.replace(/(\n|\t)/gm, ''))
+}
+
+/**
+ * Fetch the current diagrams distribution cycle numbers (.e.g, 1813)
+ */
+const fetchCurrentCycleCode = async () => {
+  const cycle = await fetchCycle('Current')
+  if (!cycle) {
+    console.warn('Current cycle not found or not available.')
+    return null
+  }
+  return cycle.val
+}
+
+chartSupplements.fetchCurrentCycleCode = fetchCurrentCycleCode
+
+/**
+ * Fetch the current diagrams distribution cycle numbers (.e.g, 1813)
+ */
+const fetchNextCycleCode = async () => {
+  const cycle = await fetchCycle('Next')
+  if (!cycle) {
+    console.warn('Next cycle not found or not available. The Next cycle is available 19 days before the end of the current cycle.')
+    return null
+  }
+  return cycle.val
+}
+
+chartSupplements.fetchNextCycleCode = fetchNextCycleCode
 
 /**
  * Fetch chart supplements for a single ICAO code
  */
-const listOne = (icao) => {
-  return fetchCurrentCycle().then(searchCycle => {
-    return superagent.get(`${BASE_URL}results/?cycle=${searchCycle}&ident=${icao}&navaid=`)
-      .then(res => parse(res.text))
-  })
+const listOne = async (icao, options) => {
+  let searchCycle = null
+  
+  if (options.getNextCycle === true) {
+    searchCycle = await fetchNextCycleCode()
+  }
+
+  // If the next cycle is not requested, or it is, but it is not
+  // available, default to the current cycle
+  if (searchCycle === null) {
+    if (options.getNextCycle === true) {
+      console.warn('Next cycle not available. Retrieving current cycle instead.')
+    }
+    searchCycle = await fetchCurrentCycleCode()
+  }
+
+  // Build up a base set of query params
+  let urlParams = [ `ident=${icao}`, ]
+  // The searchCycle is optional as the API assumes the latest already
+  // and this function uses the latest cycle
+  if (searchCycle) {
+    urlParams.push(`cycle=${searchCycle}`)
+  }
+  
+  const res = await superagent.get(`${BASE_URL}results/?cycle=${searchCycle}&ident=${icao}&navaid=`)
+  return parse(res.text)
+}
+
+/**
+ * Parsing helper methods
+ */
+const text = ($row, columnIndex) =>
+  $row
+    .find(`td:nth-child(${columnIndex})`)
+    .text()
+    .trim()
+
+const link = ($row, columnIndex) =>
+  $row
+    .find(`td:nth-child(${columnIndex})`)
+    .find('a')
+    .attr('href')
+
+/**
+ * Extract the relevant information from the dom node and return
+ * an object with the data mapped by the appropriate named key
+ * @param {HTMLNode} $row - The dom node that contains the tabular data
+ */
+const extractRow = ($row) => {
+  const type = text($row, 7)
+
+  if (!type) {
+    return null
+  }
+
+  return {
+    ident: text($row, 1),
+    city: text($row, 2),
+    state: text($row, 3),
+    airport: text($row, 4),
+    navAid: text($row, 5),
+    chart: text($row, 6),
+    volBackPages: {
+      name: text($row, 7),
+      url: link($row, 7)
+    },
+    airportNavAidListing: {
+      name: text($row, 8),
+      url: link($row, 8)
+    }
+  }
 }
 
 /**
@@ -44,35 +186,66 @@ const listOne = (icao) => {
  */
 const parse = (html) => {
   const $ = cheerio.load(html)
-  const $results = $('#resultsTable')
+  const $resultsTable = $('#resultsTable')
+  const noResultsFound = $('.message-box.info').text().trim()
 
-  if (!$results.html()) {
+  if (!!noResultsFound && noResultsFound === 'No results found.') {
+    console.warn(noResultsFound)
     return null
   }
+  else if (!$resultsTable.html()) {
+    console.error('Unable to parse the #resultsTable page element')
+    return null
+  }
+  
+  const { effectiveStartDate, effectiveEndDate } = extractEffectiveDates($)
+  
+  const results = $resultsTable
+    .find('tr')
+    .toArray()
+    .map(row => {
+      var row = extractRow($(row))
+      if (!row) {
+        return row
+      }
+      return {
+        ...row,
+        effectiveStartDate,
+        effectiveEndDate
+      }
+    })
+    .filter(x => !!x)
 
-  const ident = $results.find('td:nth-child(1)').text().trim()
-  const city = $results.find('td:nth-child(2)').text().trim()
-  const state = $results.find('td:nth-child(3)').text().trim()
-  const airport = $results.find('td:nth-child(4)').text().trim()
-  const navAid = $results.find('td:nth-child(5)').text().trim()
-  const chart = $results.find('td:nth-child(6)').text().trim()
-  const volBackPages = {
-    name: $results.find('td:nth-child(7)').text().trim(),
-    url: $results.find('td:nth-child(7)').find('a').attr('href')
-  }
-  const airportNavAidListing = {
-    name: $results.find('td:nth-child(8)').text().trim(),
-    url: $results.find('td:nth-child(8)').find('a').attr('href')
-  }
+  return results
+}
 
-  return {
-    ident,
-    city,
-    state,
-    airport,
-    navAid,
-    chart,
-    volBackPages,
-    airportNavAidListing
+/**
+ * Scrape the Effective date range from the dom
+ * @param {Object} $ - The Cheerio object that contains the serialized dom
+ * @returns {Object} - An object containing the effective start and end date
+ */
+const extractEffectiveDates = $ => {
+  const baseEffectiveDateString = $('.resultsSummary .join').html()
+  .split(':')[1]
+  .split('<')[0]
+  .trim()
+
+  return parseEffectiveDates(baseEffectiveDateString)
+}
+
+const parseEffectiveDates = str => {
+  if (!str) {
+    return null
   }
+  const [ startMonthDay, remainder ] = str.split('-')
+  const [ endMonthDay, yearAndCycle ] = remainder.split(',')
+  const [ year, _ ] = yearAndCycle.split('[')
+
+  const effectiveStartDate = new Date(`${startMonthDay.trim()} ${year}`)
+  effectiveStartDate.setUTCHours(0,0,0,0)
+
+  const effectiveEndDate = new Date(`${endMonthDay.trim()} ${year}`)
+  effectiveEndDate.setUTCHours(0,0,0,0)
+
+  return { effectiveStartDate, effectiveEndDate }
 }
